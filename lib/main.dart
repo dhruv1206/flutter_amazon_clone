2488b78a78b04a42afca75be0096dca34c5bd278
Dhruv <agrawaldhruv1006@gmail.com>
2023-01-09 20:38:17 +0530

    Done with setting up project and themes.

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text('Flutter Demo Home Page'),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("CLick"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
