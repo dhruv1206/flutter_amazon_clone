856374643515a582c4144a65cc9222779ce7e8af
Dhruv <agrawaldhruv1006@gmail.com>
2023-01-12 23:39:18 +0530

    Create Sign Up Route

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 // IMPORTS FROM PACKAGES
 const express = require("express");
+const mongoose = require("mongoose");
 // IMPORTS FROM OTHER FILES
 const authRouter = require("./routes/auth");
 
 // INIT
 const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://dhruv1206:dhruv_1206@cluster0.lhwdagb.mongodb.net/?retryWrites=true&w=majority";
 
 // middleware
+app.use(express.json());
 app.use(authRouter);
+
+mongoose.connect(DB).then(() => {
+  console.log("Connection successfull with MongoDB");
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`connected at port ${PORT}`);
