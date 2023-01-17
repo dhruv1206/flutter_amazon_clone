d8c241f3e8586657477a495432da73cddcdae01d
Dhruv <agrawaldhruv1006@gmail.com>
2023-01-18 00:27:56 +0530

    Account Screen UI completed.

---
+++ lib/features/accounts/widgets/orders.dart
@@ -0,0 +1,66 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/accounts/widgets/single_product.dart';
+import "package:flutter/material.dart";
+import 'package:flutter/rendering.dart';
+
+class Orders extends StatefulWidget {
+  const Orders({super.key});
+
+  @override
+  State<Orders> createState() => _OrdersState();
+}
+
+class _OrdersState extends State<Orders> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text(
+                "Your Orders",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: TextButton(
+                child: Text(
+                  "See all",
+                  style: TextStyle(
+                    color: GlobalVariables.selectedNavBarColor,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+        //display orders
+        Container(
+          height: 170,
+          padding: const EdgeInsets.only(
+            left: 10,
+            top: 20,
+            right: 0,
+          ),
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return SingleProduct(
+                  image:
+                      "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxMXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60");
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
